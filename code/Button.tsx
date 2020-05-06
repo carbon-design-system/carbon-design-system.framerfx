6d1f81bccb349978c53acff20b9488617f5d0be9
Aroa GB <aroa@framer.com>
2020-05-06 11:34:24 +0200

    change button
    - Add onTap event handler
    - Remove stack in order to test autosizing
    - change height 40

--- code/Button.tsx
+++ code/Button.tsx
@@ -4,56 +4,67 @@ import { addPropertyControls, ControlType, Stack } from "framer"
 import { omitIrrelevantProps } from "./utils/props"
 
 export function Button(props) {
-  const { text, isLoading, ...rest } = omitIrrelevantProps(props)
+    const { text, isLoading, tap, ...rest } = omitIrrelevantProps(props)
 
-  if (isLoading) {
-    return <System.ButtonSkeleton style={{ width: rest.width, height: rest.height }} />
-  }
+    if (isLoading) {
+        return (
+            <System.ButtonSkeleton
+                style={{ width: rest.width, height: rest.height }}
+                onTap={() => tap()}
+            />
+        )
+    }
 
-  return (
-    <Stack distribution="center" size="100%">
-      <System.Button {...rest}>{text}</System.Button>
-    </Stack>
-  )
+    return (
+        // <Stack distribution="center" size="100%">
+        <System.Button {...rest} onTap={() => tap()}>
+            {text}
+        </System.Button>
+        // </Stack>
+    )
 }
 
 //Define default props
 Button.defaultProps = {
-  width: 118,
-  height: 48,
+    width: 118,
+    height: 40,
 }
 
 //Create property controls to expose properties in the canvas
 addPropertyControls(Button, {
-  text: {
-    type: ControlType.String,
-    title: "Text",
-    defaultValue: "Button",
-  },
-  kind: {
-    type: ControlType.Enum,
-    title: "Kind",
-    options: ["primary", "secondary", "tertiary", "danger"],
-    optionTitles: ["Primary", "Secondary", "Tertiary", "Danger"],
-    defaultValue: "primary",
-  },
-  size: {
-    type: ControlType.Enum,
-    title: "Size",
-    options: ["field", "small"],
-    optionTitles: ["Field", "Small"],
-    defaultValue: "field",
-  },
-  disabled: {
-    type: ControlType.Boolean,
-    title: "Disabled",
-    enabledTitle: "Yes",
-    disabledTitle: "No",
-    defaultValue: false,
-  },
-  isLoading: {
-    title: "Loading",
-    type: ControlType.Boolean,
-    defaultValue: false,
-  },
+    text: {
+        type: ControlType.String,
+        title: "Text",
+        defaultValue: "Button",
+    },
+    kind: {
+        type: ControlType.Enum,
+        title: "Kind",
+        options: ["primary", "secondary", "tertiary", "danger"],
+        optionTitles: ["Primary", "Secondary", "Tertiary", "Danger"],
+        defaultValue: "primary",
+    },
+    size: {
+        type: ControlType.Enum,
+        title: "Size",
+        options: ["field", "small"],
+        optionTitles: ["Field", "Small"],
+        defaultValue: "field",
+    },
+    disabled: {
+        type: ControlType.Boolean,
+        title: "Disabled",
+        enabledTitle: "Yes",
+        disabledTitle: "No",
+        defaultValue: false,
+    },
+    isLoading: {
+        title: "Loading",
+        type: ControlType.Boolean,
+        defaultValue: false,
+    },
+    tap: {
+        type: ControlType.EventHandler,
+        title: "On Tap",
+    },
 })
