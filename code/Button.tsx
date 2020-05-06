9f94139c3dcb87f761e6ccc71ccf2cfcc5f58492
iKettles <iain@framer.com>
2020-05-06 11:43:50 +0200

    Clean up onTap handler naming and memoise callback

--- code/Button.tsx
+++ code/Button.tsx
@@ -1,70 +1,70 @@
 import * as React from "react"
 import * as System from "carbon-components-react"
-import { addPropertyControls, ControlType, Stack } from "framer"
+import { addPropertyControls, ControlType } from "framer"
 import { omitIrrelevantProps } from "./utils/props"
 
 export function Button(props) {
-    const { text, isLoading, tap, ...rest } = omitIrrelevantProps(props)
+  const { text, isLoading, onTap, ...rest } = omitIrrelevantProps(props)
 
-    if (isLoading) {
-        return (
-            <System.ButtonSkeleton
-                style={{ width: rest.width, height: rest.height }}
-                onTap={() => tap()}
-            />
-        )
+  const onButtonTapped = React.useCallback(() => {
+    if (onTap) {
+      onTap()
     }
+  }, [onTap])
 
-    return (
-        // <Stack distribution="center" size="100%">
-        <System.Button {...rest} onTap={() => tap()}>
-            {text}
-        </System.Button>
-        // </Stack>
-    )
+  if (isLoading) {
+    return <System.ButtonSkeleton style={{ width: rest.width, height: rest.height }} onTap={onButtonTapped} />
+  }
+
+  return (
+    <System.Button {...rest} onTap={onButtonTapped}>
+      {text}
+    </System.Button>
+  )
 }
 
 //Define default props
 Button.defaultProps = {
-    width: 118,
-    height: 40,
+  width: 118,
+  height: 40,
+  onTap: () => console.log("Button Tapped!"),
 }
 
 //Create property controls to expose properties in the canvas
 addPropertyControls(Button, {
-    text: {
-        type: ControlType.String,
-        title: "Text",
-        defaultValue: "Button",
-    },
-    kind: {
-        type: ControlType.Enum,
-        title: "Kind",
-        options: ["primary", "secondary", "tertiary", "danger"],
-        optionTitles: ["Primary", "Secondary", "Tertiary", "Danger"],
-        defaultValue: "primary",
-    },
-    size: {
-        type: ControlType.Enum,
-        title: "Size",
-        options: ["field", "small"],
-        optionTitles: ["Field", "Small"],
-        defaultValue: "field",
-    },
-    disabled: {
-        type: ControlType.Boolean,
-        title: "Disabled",
-        enabledTitle: "Yes",
-        disabledTitle: "No",
-        defaultValue: false,
-    },
-    isLoading: {
-        title: "Loading",
-        type: ControlType.Boolean,
-        defaultValue: false,
-    },
-    tap: {
-        type: ControlType.EventHandler,
-        title: "On Tap",
-    },
+  text: {
+    type: ControlType.String,
+    title: "Text",
+    defaultValue: "Button",
+  },
+  kind: {
+    type: ControlType.Enum,
+    title: "Kind",
+    options: ["primary", "secondary", "tertiary", "danger"],
+    optionTitles: ["Primary", "Secondary", "Tertiary", "Danger"],
+    defaultValue: "primary",
+  },
+  size: {
+    type: ControlType.Enum,
+    title: "Size",
+    options: ["field", "small"],
+    optionTitles: ["Field", "Small"],
+    defaultValue: "field",
+  },
+  disabled: {
+    type: ControlType.Boolean,
+    title: "Disabled",
+    enabledTitle: "Yes",
+    disabledTitle: "No",
+    defaultValue: false,
+  },
+  isLoading: {
+    title: "Loading",
+    type: ControlType.Boolean,
+    defaultValue: false,
+  },
+  onTap: {
+    type: ControlType.EventHandler,
+    title: "On Tap",
+  },
 })
