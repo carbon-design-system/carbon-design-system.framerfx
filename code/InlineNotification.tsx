92cf98cfb0f660b71051c5b4268428b420b53f34
iKettles <iain@framer.com>
2020-07-29 16:00:36 +0100

    Add InlineNotification event handlers

--- code/InlineNotification.tsx
+++ code/InlineNotification.tsx
@@ -7,14 +7,33 @@ import { startCase } from "./utils"
 
 // @TODO Fix inconsistencies between preview and canvas
 const InnerInlineNotification = (props) => {
-  const { actions, ...rest } = omitIrrelevantProps(props)
+  const { actions, onClose, onAction1Click, onAction2Click, onAction3Click, ...rest } = omitIrrelevantProps(props)
+  const onClickAction = React.useCallback(
+    (index: number) => {
+      switch (index) {
+        case 0:
+          onAction1Click()
+          break
+        case 1:
+          onAction2Click()
+          break
+        case 2:
+          onAction3Click()
+          break
+      }
+    },
+    [onAction1Click, onAction2Click, onAction3Click]
+  )
   return (
     <System.InlineNotification
       {...rest}
       actions={actions.map((action, index) => (
-        <System.NotificationActionButton key={index}>{action}</System.NotificationActionButton>
+        <System.NotificationActionButton key={index} onClick={() => onClickAction(index)}>
+          {action}
+        </System.NotificationActionButton>
       ))}
-    ></System.InlineNotification>
+      onCloseButtonClick={onClose}
+    />
   )
 }
 
@@ -23,6 +42,10 @@ export const InlineNotification = withHOC(InnerInlineNotification)
 InlineNotification.defaultProps = {
   width: 320,
   height: 82,
+  onClose: () => console.log(`Notification Closed`),
+  onAction1Click: () => console.log(`Action 1 Clicked`),
+  onAction2Click: () => console.log(`Action 2 Clicked`),
+  onAction3Click: () => console.log(`Action 3 Clicked`),
 }
 
 const kindOptions = ["error", "info", "success", "warning"]
@@ -63,5 +86,17 @@ addPropertyControls(InlineNotification, {
       type: ControlType.String,
     },
     defaultValue: ["Action"],
+  },
+  onClose: {
+    type: ControlType.EventHandler,
+  },
+  onAction1Click: {
+    type: ControlType.EventHandler,
+  },
+  onAction2Click: {
+    type: ControlType.EventHandler,
+  },
+  onAction3Click: {
+    type: ControlType.EventHandler,
   },
 })
