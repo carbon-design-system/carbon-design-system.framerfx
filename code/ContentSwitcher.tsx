a2bf9c7a689cf91c0429ed68df1fc87478246041
iKettles <iain@framer.com>
2020-07-30 14:59:25 +0100

    Add event handlers for ContentSwitcher sections

--- code/ContentSwitcher.tsx
+++ code/ContentSwitcher.tsx
@@ -5,10 +5,56 @@ import { withHOC } from "./withHOC"
 import { omitIrrelevantProps } from "./utils/props"
 
 const InnerContentSwitcher = (props) => {
-  const { sections, ...rest } = omitIrrelevantProps(props)
+  const {
+    sections,
+    onClickSection1,
+    onClickSection2,
+    onClickSection3,
+    onClickSection4,
+    onClickSection5,
+    onClickSection6,
+    onChange,
+    ...rest
+  } = omitIrrelevantProps(props)
+
+  const onSelectionChange = React.useCallback(
+    (value: { index: number; name: string; text: string }) => {
+      let toCall: () => void
+
+      switch (value.index) {
+        case 0:
+          toCall = onClickSection1
+          break
+        case 1:
+          toCall = onClickSection2
+          break
+        case 2:
+          toCall = onClickSection3
+          break
+        case 3:
+          toCall = onClickSection4
+          break
+        case 4:
+          toCall = onClickSection5
+          break
+        case 5:
+          toCall = onClickSection6
+          break
+      }
+
+      if (toCall) {
+        toCall()
+      }
+
+      if (onChange) {
+        onChange(value)
+      }
+    },
+    [onChange, onClickSection1, onClickSection2, onClickSection3, onClickSection4, onClickSection5, onClickSection6]
+  )
 
   return (
-    <System.ContentSwitcher {...rest}>
+    <System.ContentSwitcher {...rest} onChange={onSelectionChange}>
       {sections.map((section, index) => (
         <System.Switch key={`${section}-${index}`} name={`${index}`} text={section} />
       ))}
@@ -21,7 +67,6 @@ export const ContentSwitcher = withHOC(InnerContentSwitcher)
 ContentSwitcher.defaultProps = {
   width: 410,
   height: 40,
-  onChange: (value) => console.log(value),
 }
 
 addPropertyControls(ContentSwitcher, {
@@ -32,5 +77,23 @@ addPropertyControls(ContentSwitcher, {
       type: ControlType.String,
     },
     defaultValue: ["First Section", "Second Section", "Third Section"],
+  },
+  onClickSection1: {
+    type: ControlType.EventHandler,
+  },
+  onClickSection2: {
+    type: ControlType.EventHandler,
+  },
+  onClickSection3: {
+    type: ControlType.EventHandler,
+  },
+  onClickSection4: {
+    type: ControlType.EventHandler,
+  },
+  onClickSection5: {
+    type: ControlType.EventHandler,
+  },
+  onClickSection6: {
+    type: ControlType.EventHandler,
   },
 })
