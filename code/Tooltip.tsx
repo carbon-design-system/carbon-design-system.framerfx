089e4eb7b5a623302c9621c36603c0707f283cdb
iKettles <iain@framer.com>
2020-07-30 14:13:53 +0100

    Fix content alignment issues, add props for icon, use text area for tooltip text

--- code/Tooltip.tsx
+++ code/Tooltip.tsx
@@ -4,12 +4,19 @@ import { ControlType, PropertyControls, addPropertyControls } from "framer"
 import { withHOC } from "./withHOC"
 import { startCase } from "./utils"
 import { omitIrrelevantProps } from "./utils/props"
+import { ensureRelativePositioning } from "./utils/layout"
 
 const InnerTooltip = (props) => {
-  const { content, tooltipText, ...rest } = omitIrrelevantProps(props)
+  const { content, tooltipText, open, ...rest } = omitIrrelevantProps(props)
   return (
-    <System.Tooltip {...rest}>
-      <div style={{ position: "relative" }}>{content && content.length ? content : <p>{tooltipText}</p>}</div>
+    <System.Tooltip {...rest} open={!open ? undefined : open}>
+      <div style={{ position: "relative" }}>
+        {content && content.length ? (
+          ensureRelativePositioning(React.Children.toArray(content)[0])
+        ) : (
+          <p>{tooltipText}</p>
+        )}
+      </div>
     </System.Tooltip>
   )
 }
@@ -17,27 +24,52 @@ const InnerTooltip = (props) => {
 export const Tooltip = withHOC(InnerTooltip)
 
 Tooltip.defaultProps = {
-  width: 98,
+  width: 101,
   height: 17,
 }
 
 const directionOptions = ["bottom", "top", "left", "right"]
+const typeOptions = ["text", "content"]
 
 addPropertyControls(Tooltip, {
+  triggerText: {
+    title: "Label",
+    type: ControlType.String,
+    defaultValue: "Tooltip Label",
+  },
+  type: {
+    title: "Type",
+    type: ControlType.SegmentedEnum,
+    options: typeOptions,
+    optionTitles: typeOptions.map(startCase),
+    defaultValue: "text",
+  },
   tooltipText: {
     title: "Text",
     type: ControlType.String,
     defaultValue:
       "This is some tooltip text. This box shows the maximum amount of text that should appear inside. If more room is needed please use a modal instead.",
+    displayTextArea: true,
+    hidden: (props: any) => props.type !== "text",
   },
   content: {
     title: "Content",
     type: ControlType.ComponentInstance,
+    hidden: (props: any) => props.type !== "content",
   },
-  triggerText: {
-    title: "Tooltip Label",
-    type: ControlType.String,
-    defaultValue: "Tooltip Label",
+  showIcon: {
+    title: "Icon",
+    type: ControlType.Boolean,
+    enabledTitle: "Show",
+    disabledTitle: "Hide",
+    defaultValue: true,
+  },
+  open: {
+    title: "State",
+    type: ControlType.Boolean,
+    enabledTitle: "Open",
+    disabledTitle: "Closed",
+    defaultValue: false,
   },
   direction: {
     title: "Direction",
