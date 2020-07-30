3f8ce630c974dc6a18caa7fb479c74ea4e859284
iKettles <iain@framer.com>
2020-07-30 13:41:51 +0100

    Fix rendering glitches, disable timeout on canvas, add onClose event handler

--- code/ToastNotification.tsx
+++ code/ToastNotification.tsx
@@ -1,13 +1,37 @@
 import * as React from "react"
 import * as System from "carbon-components-react"
-import { ControlType, PropertyControls, addPropertyControls } from "framer"
+import { ControlType, PropertyControls, addPropertyControls, RenderTarget } from "framer"
 import { withHOC } from "./withHOC"
 import { startCase } from "./utils"
 import { omitIrrelevantProps } from "./utils/props"
 
+const timeoutRenderTargets = [RenderTarget.preview, RenderTarget.export]
+
 const InnerToastNotification = (props) => {
-  const { ...rest } = omitIrrelevantProps(props)
-  return <System.ToastNotification {...rest}></System.ToastNotification>
+  const { hideSubtitle, subtitle, hideCaption, caption, timeout, onClose, ...rest } = omitIrrelevantProps(props)
+  const subtitleText = React.useMemo(() => (hideSubtitle ? "" : subtitle), [hideSubtitle, subtitle])
+  const captionText = React.useMemo(() => (hideCaption ? "" : caption), [hideCaption, caption])
+  /**
+   * Set the key based on caption/subtitle — this forces the component to re-mount
+   * to avoid UI glitches on the canvas when changing properties.
+   */
+  const key = React.useMemo(() => `${subtitleText}-${captionText}`, [subtitleText, captionText])
+  const onNotificationClosed = React.useCallback(() => {
+    if (onClose) {
+      onClose()
+    }
+  }, [onClose])
+
+  return (
+    <System.ToastNotification
+      {...rest}
+      key={key}
+      subtitle={subtitleText}
+      caption={captionText}
+      timeout={timeoutRenderTargets.indexOf(RenderTarget.current()) > -1 ? timeout : undefined}
+      onClose={onNotificationClosed}
+    ></System.ToastNotification>
+  )
 }
 
 export const ToastNotification = withHOC(InnerToastNotification)
@@ -25,15 +49,31 @@ addPropertyControls(ToastNotification, {
     type: ControlType.String,
     defaultValue: "Toast Notification Title",
   },
-  subtitle: {
+  hideSubtitle: {
     title: "Subtitle",
+    type: ControlType.Boolean,
+    enabledTitle: "Hide",
+    disabledTitle: "Show",
+    defaultValue: false,
+  },
+  subtitle: {
+    title: "↳ Text",
     type: ControlType.String,
     defaultValue: "Notification Subtitle",
+    hidden: (props: any) => props.hideSubtitle,
+  },
+  hideCaption: {
+    title: "Caption",
+    type: ControlType.Boolean,
+    enabledTitle: "Hide",
+    disabledTitle: "Show",
+    defaultValue: false,
   },
   caption: {
-    title: "Caption",
+    title: "↳ Text",
     type: ControlType.String,
     defaultValue: "Notification Caption",
+    hidden: (props: any) => props.hideCaption,
   },
   kind: {
     title: "Kind",
@@ -42,17 +82,17 @@ addPropertyControls(ToastNotification, {
     options: kindOptions,
     optionTitles: kindOptions.map(startCase),
   },
-  lowContrast: {
-    title: "Low contrast",
-    type: ControlType.Boolean,
-    defaultValue: false,
-  },
   hideCloseButton: {
     title: "Close Button",
     type: ControlType.Boolean,
     defaultValue: false,
     enabledTitle: "Hide",
     disabledTitle: "Show",
+  },
+  lowContrast: {
+    title: "Low contrast",
+    type: ControlType.Boolean,
+    defaultValue: false,
   },
   timeout: {
     title: "Timeout",
@@ -61,5 +101,8 @@ addPropertyControls(ToastNotification, {
     min: 0,
     displayStepper: true,
     unit: "ms",
+  },
+  onClose: {
+    type: ControlType.EventHandler,
   },
 })
